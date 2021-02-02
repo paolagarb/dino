1fbf0ee1a6a59533a0e350e8ccac76a58c255922
Paola Garbato <paolagarbato@gmail.com>
2021-02-02 13:41:41 -0300

    Adicionando cactus - gerando valor aleatório para intervalo entre eles

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const dino = document.getElementsByClassName('dino');
-let isJumping = false;
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 
-function handleyKeyUp(event) {
+let isJumping = false;
+let position = 0;
+
+function handleKeyUp(event) {
     if (event.keyCode === 32) { //32 = espaço
         if (!isJumping) {
             jump();
@@ -9,37 +12,61 @@ function handleyKeyUp(event) {
     }
 }
 
-function jump() {
-    let position = 0;
+function jump() { 
     isJumping = true;
 
-    if (position >= 150) 
-    {
-        clearInterval(upInterval);
+    let upInterval = setInterval(() => {
+        if (position >= 150) 
+        {
+            clearInterval(upInterval);
 
-        let downInterval = setInterval(() => {
-            if (position <= 0) 
-            {
-                clearInterval(downInterval());
-                isJumping = false;
-            } 
-            else 
-            {
-                position -= 20;
-                dino.style.bottom = position + 'px';
-            }
-        }, 20);
-
-    } 
-    else 
-    {
-        let upInterval = setInterval(() => {
-        position += 20;
-
-        dino.style.bottom = position + 'px';
-
+            let downInterval = setInterval(() => {
+                if (position <= 0) 
+                {
+                    clearInterval(downInterval());
+                    isJumping = false;
+                } 
+                else 
+                {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } 
+        else 
+        {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
     }, 20);
-    }
 }
 
-document.addEventListener('keyup', handleyKeyUp);
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+    
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) 
+        {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }
+        else 
+        {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';    
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
